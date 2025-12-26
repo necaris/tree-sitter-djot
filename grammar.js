75c2d2bfaa10db3bb21200dade2356fa107d2ff7
Rami Chowdhury <rami@skyfi.com>
2025-12-26 13:21:58 -0500

    feat: implement Djot attributes system
    Add comprehensive support for Djot attributes {#id .class key=value}:
    - Attribute IDs (#myid)
    - Attribute classes (.class1 .class2)
    - Key-value pairs (key=value, key="quoted value", key='quoted')
    - Whitespace handling between attribute components
    
    Attributes can be applied to:
    - Block elements (divs, code blocks)
    - Inline elements (emphasis)
    
    Added 10 comprehensive test cases covering:
    - Individual attribute types (ID, class, key-value)
    - Mixed attributes
    - Quoted and unquoted values
    - Attributes on different element types
    
    All tests passing (14/14).
    
    Ref: tree-sitter-djot-564.25

--- grammar.js
+++ grammar.js
@@ -14,6 +14,43 @@ module.exports = grammar({
     // All blocks should end with a newline, but we can also parse multiple newlines.
     _block: ($) => choice($.div, $.code_block, $.paragraph, "\n"),
 
+    // AIDEV-NOTE: Attributes system {#id .class key=value}
+    // Can be applied to both block and inline elements
+    attributes: ($) =>
+      seq(
+        "{",
+        optional(/\s+/),
+        repeat(
+          seq(
+            choice(
+              $.attribute_id,
+              $.attribute_class,
+              $.attribute_key_value
+            ),
+            optional(/\s+/)
+          )
+        ),
+        "}"
+      ),
+
+    attribute_id: (_) => seq("#", /[^\s}]+/),
+    attribute_class: (_) => seq(".", /[^\s}]+/),
+    attribute_key_value: ($) =>
+      seq(
+        $.attribute_key,
+        "=",
+        choice(
+          $.attribute_value_quoted,
+          $.attribute_value_unquoted
+        )
+      ),
+    attribute_key: (_) => /[a-zA-Z_:][a-zA-Z0-9_:.-]*/,
+    attribute_value_quoted: (_) => token(choice(
+      seq('"', /[^"]*/, '"'),
+      seq("'", /[^']*/, "'")
+    )),
+    attribute_value_unquoted: (_) => token(/[^\s}'"]+/),
+
     // A div contains other blocks.
     div: ($) =>
       prec.left(
@@ -21,6 +58,7 @@ module.exports = grammar({
           // A rule starting with "_" will be hidden in the output,
           // and we can use "alias" to rename rules.
           alias($._div_marker_begin, $.div_marker),
+          optional($.attributes),
           "\n",
           repeat($._block),
           $._block_close,
@@ -33,13 +71,14 @@ module.exports = grammar({
       seq(
         $.code_block_marker,
         optional($.language),
+        optional($.attributes),
         "\n",
         optional($.code),
         $.code_block_marker
       ),
     code_block_marker: (_) => "```",
     code: (_) => repeat1(seq(/[^\n]*/, "\n")),
-    language: (_) => /[^\s]+/,
+    language: (_) => /[^\s{]+/,
 
     // A paragraph contains inline content and is terminated by a blankline
     // (two newlines in a row) or by a div marker.
@@ -49,7 +88,7 @@ module.exports = grammar({
     // The markup parser could separate block and inline parsing into separate steps,
     // but we'll do everything in one parser.
     _inline: ($) => repeat1(choice($.emphasis, $._text, $._fallback)),
-    emphasis: ($) => prec.left(seq("_", $._inline, "_")),
+    emphasis: ($) => prec.left(seq("_", $._inline, "_", optional($.attributes))),
     // prec.dynamic() is used during conflict resolution to choose which
     // branch to choose if multiple succeed.
     _fallback: (_) => prec.dynamic(-100, "_"),
